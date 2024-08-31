0fd2a066c800fb4b8b4b3140bd4caf813c1afe2c
ziko278 <odekeziko@gmail.com>
2024-08-31 08:21:37 +0100

    updated

--- crypto_investment/settings.py
+++ crypto_investment/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'admin_site.middleware.AdminUserRestrictionMiddleware',
 ]
 
 ROOT_URLCONF = 'crypto_investment.urls'
